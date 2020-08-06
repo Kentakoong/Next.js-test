c3bf19f76e687ddac9d12dcdaafef5fb1f55bad9
Wongkraiwich Chuenchomphu <35761701+Kentakoong@users.noreply.github.com>
2020-08-06 08:56:10 +0700

    Update details.js

--- next-playsound/src/pages/details.js
+++ next-playsound/src/pages/details.js
@@ -1,3 +1,19 @@
+import Link from 'next/link';
+
+const phone = [
+    {p: 'iphonex', name: 'banana'},
+    {p: 'iphone11', name: 'henlo'},
+    {p: 'iphone12', name: 'apple'}
+]
+
 export default function Details() {
-    return <h2>Details</h2>
+    return <div>
+        {phone.map(e => (
+            <div>
+        <Link as={'/${e.p}/${e.name}'} href="/[phone]/[person]">
+            <a>Navigate to {e.name}'s {e.p}</a>
+        </Link>
+        </div>
+        ))}
+    </div>
 }
